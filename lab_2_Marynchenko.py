bce2d4acd140f527b1b15dda73b4ecef201d5885
ValeriiaMarynchenko <125971218+ValeriiaMarynchenko@users.noreply.github.com>
2023-03-19 22:32:14 +0300

    Add files via upload

---
+++ lab_2_Marynchenko.py
@@ -0,0 +1,30 @@
+print("Programming", "Essential", "in", sep="***", end="...")
+print("Python")
+
+print()
+
+print("     *")
+print("    * *")
+print("   *   *")
+print("  *     *")
+print(" ***   ***")
+print("   *   *")
+print("   *   *")
+print("   *****")
+
+print()
+
+print("I'm student")
+print('I\'m student')
+
+print()
+
+print("\"I'm\" \n""\"\"learning\"\" \n\"\"\"Python\"\"\"")
+
+print()
+
+print(0o500)
+
+print()
+
+print(0x777)
